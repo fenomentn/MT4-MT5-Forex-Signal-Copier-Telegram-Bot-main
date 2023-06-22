3a75e277c429e94dadb12c7382f78d0a324a837d
fenomentn <nassimxmax@gmail.com>
2023-06-22 18:40:35 +0100

    test

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 CALCULATE, TRADE, DECISION = range(3)
 
 # allowed FX symbols
-SYMBOLS = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NOW', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY', 'XAGUSD', 'XAUUSDm']
+SYMBOLS = ['AUDCADm', 'AUDCHFm', 'AUDJPYm', 'AUDNZDm', 'AUDUSDm', 'CADCHFm', 'CADJPYm', 'CHFJPYm', 'EURAUDm', 'EURCADm', 'EURCHFm', 'EURGBPm', 'EURJPYm', 'EURNZDm', 'EURUSDm', 'GBPAUDm', 'GBPCADm', 'GBPCHFm', 'GBPJPYm', 'GBPNZDm', 'GBPUSDm', 'NOW', 'NZDCADm', 'NZDCHFm', 'NZDJPYm', 'NZDUSDm', 'USDCADm', 'USDCHFm', 'USDJPYm', 'XAGUSDm', 'XAUUSDm']
 
 # RISK FACTOR
 RISK_FACTOR = float(os.environ.get("RISK_FACTOR"))
@@ -116,7 +116,7 @@ def GetTradeInformation(update: Update, trade: dict, balance: float) -> None:
     if(trade['Symbol'] == 'XAUUSDm'):
         multiplier = 0.1
 
-    elif(trade['Symbol'] == 'XAGUSD'):
+    elif(trade['Symbol'] == 'XAGUSDm'):
         multiplier = 0.001
 
     elif(str(trade['Entry']).index('.') >= 2):
